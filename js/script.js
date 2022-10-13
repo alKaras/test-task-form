f3dc90c967e4ac1b76e68a5465e5d525f671dd9b
Oleksandr Karas <al.karas.pr@gmail.com>
2022-10-13 11:26:33 +0300

    add logic to buttons

--- js/script.js
+++ js/script.js
@@ -11,13 +11,29 @@ const cancelBtn = document.querySelector(".cancel-btn");
 const form = document.querySelector(".registration");
 
 let message = document.querySelector("#user-email");
+let greetmsg = document.querySelector("#g-user-email");
 let userEmail = document.getElementById("email");
 let userPass = document.getElementById("password");
 submitBtn.addEventListener('click', () => {
-    if (userEmail.value !== "" && userPass.value !== ""){
+
+    if (userEmail.value !== "" && userPass.value !== "") {
+        event.preventDefault();
         confirmationModal.style.display = 'block';
         message.textContent = userEmail.value;
     }
 })
 
+cancelBtn.addEventListener('click', () => {
+    event.preventDefault();
+    confirmationModal.style.display = 'none';
+    userEmail.value = "";
+    userPass.value = "";
+})
 
+confirmBtn.addEventListener('click', () => {
+    event.preventDefault();
+    formContainer.style.display = 'none';
+    confirmationModal.style.display = 'none';
+    greetingModal.style.display = 'block';
+    greetmsg.textContent = userEmail.value;
+})
